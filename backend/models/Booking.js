4125915d481b6e074c9dda2babe34684da157bfe
Anirudh Chakraborty <preetiujjal@gmail.com>
2026-03-20 15:32:25 +0530

    Anirudh backend (#16)
    * Password Bug Error Solved
    
    * Assets Updated
    
    * Landing page update and subject assets added
    
    * Landing page update and subject assets added
    
    Pacakage Json Updated
    
    * Local Commits
    
    * status: complete done in paymentRoutes
    
    * Comments added for future reference
    
    ---------
    
    Co-authored-by: Kartik Saxena <saxenaaashu74@gmail.com>
    Co-authored-by: Learnilmworld <support@learnilmworld.com>

--- backend/models/Booking.js
+++ backend/models/Booking.js
@@ -1,51 +1,72 @@
 import mongoose from 'mongoose';
 
 const bookingSchema = new mongoose.Schema({
+
+  // Store student reference for paid bookings
   student: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
   },
+
+  // Store trainer reference for both paid and free demo bookings
   trainer: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
   },
+
+  // Store student name for free demo bookings
   studentName: {
     type: String,
     required: true
   },
+
+  // Optional email for free demo bookings
   studentEmail:{
     type: String, 
     required: false, 
     trim: true, 
     lowercase: true
   },
+
+  // Type of booking: paid or free demo
   bookingType: {
     type: String,
-    enum: ['paid', 'free_demo'],
-    default: 'paid', 
+    enum: ['private', 'group', 'free_demo'],
+    default: 'private', 
     required: true
   },
+  date: { type: String }, 
+time: { type: String },
+duration: { type: Number},
   status: {
     type: String,
     enum: ['pending', 'confirmed', 'completed', 'cancelled'],
     default: 'pending'
   },
+
+  // Payment details
   paymentStatus: {
     type: String,
     enum: ['pending', 'completed', 'failed', 'na'],//na for free demo
     default: 'pending'
   },
+
+  // For paid bookings
   paymentMethod: {
     type: String,
-    enum: ['stripe', 'fake', 'none'], //none for free demo
+    enum: ['stripe', 'fake', 'none', 'razorpay'], //none for free demo
     required: true
   },
+  
+  // Store amount only for paid bookings
   amount: {
     type: Number,
-    required: function(){this.bookingType==='paid'}
+    required: function(){ return this.bookingType==='paid'} // required only if booking is paid
   },
+
+  // Store Stripe payment details
   paymentId: String,
   paymentDetails: {
     amount: Number,
@@ -55,6 +76,8 @@ const bookingSchema = new mongoose.Schema({
     receiptUrl: String,
     processedAt: Date
   },
+
+  // Reference to the session being booked
   sessionId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Session'
